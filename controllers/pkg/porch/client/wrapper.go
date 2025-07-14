fbdbd91d2709d00a18d6835584069d81f92a707e
Mózes László Máté <laszlo.mozes@nokia.com>
2025-07-14 15:08:46 +0200

    fix empty kinds

--- controllers/pkg/porch/client/wrapper.go
+++ controllers/pkg/porch/client/wrapper.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"reflect"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,38 +21,50 @@ type clientWrapper struct {
 
 var _ client.Client = &clientWrapper{}
 
+func getKind(obj runtime.Object) string {
+	if kind := obj.GetObjectKind().GroupVersionKind().Kind; kind != "" {
+		return kind
+	}
+
+	return reflect.TypeOf(obj).Elem().Name()
+}
+
 func (c *clientWrapper) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
-	klog.Infof("Client GET %s::%s", obj.GetObjectKind().GroupVersionKind().Kind, key)
+	klog.Infof("Client GET %s::%s", getKind(obj), key)
 	return c.internalClient.Get(ctx, key, obj, opts...)
 }
 
 func (c *clientWrapper) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
-	klog.Infof("Client LIST %s", list.GetObjectKind().GroupVersionKind())
+	klog.Infof("Client LIST %s", getKind(list.(runtime.Object)))
 	return c.internalClient.List(ctx, list, opts...)
 }
 
 func (c *clientWrapper) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
-	klog.Infof("Client CREATE %s::%s", obj.GetObjectKind().GroupVersionKind().Kind, obj.GetName())
+	name := obj.GetName()
+	if name == "" {
+		name = "<unnamed>"
+	}
+	klog.Infof("Client CREATE %s::%s", getKind(obj), name)
 	return c.internalClient.Create(ctx, obj, opts...)
 }
 
 func (c *clientWrapper) Delete(ctx context.Context, obj client.Object, opts ...client.DeleteOption) error {
-	klog.Infof("Client DELETE %s::%s", obj.GetObjectKind().GroupVersionKind().Kind, obj.GetName())
+	klog.Infof("Client DELETE %s::%s", getKind(obj), obj.GetName())
 	return c.internalClient.Delete(ctx, obj, opts...)
 }
 
 func (c *clientWrapper) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
-	klog.Infof("Client UPDATE %s::%s", obj.GetObjectKind().GroupVersionKind().Kind, obj.GetName())
+	klog.Infof("Client UPDATE %s::%s", getKind(obj), obj.GetName())
 	return c.internalClient.Update(ctx, obj, opts...)
 }
 
 func (c *clientWrapper) Patch(ctx context.Context, obj client.Object, patch client.Patch, opts ...client.PatchOption) error {
-	klog.Infof("Client PATCH %s::%s", obj.GetObjectKind().GroupVersionKind().Kind, obj.GetName())
+	klog.Infof("Client PATCH %s::%s", getKind(obj), obj.GetName())
 	return c.internalClient.Patch(ctx, obj, patch, opts...)
 }
 
 func (c *clientWrapper) DeleteAllOf(ctx context.Context, obj client.Object, opts ...client.DeleteAllOfOption) error {
-	klog.Infof("Client DELETE ALL %s", obj.GetObjectKind().GroupVersionKind())
+	klog.Infof("Client DELETE ALL %s", getKind(obj))
 	return c.internalClient.DeleteAllOf(ctx, obj, opts...)
 }
 
